0ea3a0f7e8c728ca179b1258ad08ed7d624ae9d3
Sebastian Larsson <sebberlarsson@hotmail.com>
2018-11-27 22:37:44 +0100

    Start setting up webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const test = 42;
+console.log('Herllo');
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
